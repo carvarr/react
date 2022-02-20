4803f5b95113c7b4fe83fe68801f6413f4651532
Carlos Mario Villadiego <carvarr@gmail.com>
2022-02-19 21:25:15 -0500

    removed useContext import that is unused

--- food-order/src/components/Layout/Header.js
+++ food-order/src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import { Fragment, useContext } from "react";
+import { Fragment } from "react";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
 import HeaderCartButton from "./HeaderCartButton";
